fa0c561fdb43546309275bb030c6072fc5634eef
Richard Li <evilcat@wisewolfsolutions.com>
2018-03-18 05:21:19 +0000

    producer

---
+++ meetup-rsvp-producer/meetup-rsvp-producer.rb
@@ -0,0 +1,16 @@
+require 'websocket-client-simple'
+require 'kafka'
+
+kafka = Kafka.new(['localhost:29092'], client_id: 'meetup')
+
+producer = kafka.async_producer(delivery_interval: 5)
+
+ws = WebSocket::Client::Simple.connect 'ws://stream.meetup.com/2/rsvps'
+
+ws.on :message do |msg|
+  producer.produce(msg, topic: 'meetup')
+end
+
+loop do
+  sleep 10
+end
