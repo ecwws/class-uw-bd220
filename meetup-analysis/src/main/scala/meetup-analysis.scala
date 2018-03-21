a6a85eecb262c6c0f11d1355f4fb887c3f0e4852
Richard Li <evilcat@wisewolfsolutions.com>
2018-03-21 05:53:44 +0000

    mostly it

--- meetup-analysis/src/main/scala/meetup-analysis.scala
+++ meetup-analysis/src/main/scala/meetup-analysis.scala
@@ -21,8 +21,17 @@ object MeetupAnalysis {
           add("group_city", StringType)
           add("group_state", StringType)
           add("group_country", StringType)
+          add("group_topics",
+            new ArrayType(
+              new StructType()
+                add("urlkey", StringType)
+                add("topic_name", StringType),
+              true
+            )
+          )
       )
     )
+
 
     val df = (
       spark.readStream.format("kafka")
@@ -35,16 +44,39 @@ object MeetupAnalysis {
       df.select(col("key").cast("string"),
                 from_json(col("value").cast("string"), schema).alias("parsed"))
 
-    val result = (
+    val flattened = (
       data.select(
         from_unixtime((col("parsed.mtime")/1000).cast(IntegerType)).as("timestamp"),
-        col("parsed.group.group_country"),
-        col("parsed.group.group_state"),
-        col("parsed.group.group_city")
+        col("parsed.group.group_country").as("country"),
+        col("parsed.group.group_city").as("city"),
+        explode(col("parsed.group.group_topics")).as("topics")
       )
     )
 
-    val query = data.writeStream.format("console").start
+    val target = (
+      flattened
+      groupBy(
+        window(col("timestamp"), "2 minute", "1 minute").as("window"),
+        col("country"),
+        col("city"),
+        col("topics.topic_name").as("topic")
+      )
+      count()
+    )
+
+    val jsonOut = target.selectExpr("CAST(window AS STRING) AS key",
+                                    "to_json(struct(*)) AS value")
+
+    val query = (
+      jsonOut
+      writeStream()
+      outputMode("update")
+      format("kafka")
+      option("kafka.bootstrap.servers", "localhost:29092")
+      option("topic", "meetup_analysis")
+      option("checkpointLocation", "file:///tmp/kafka")
+      start
+    )
 
     query.awaitTermination
   }
